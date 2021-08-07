2718646505d015c499c98ce702e03bcbb18b9b05
phumaster <449294-phumaster@users.noreply.gitlab.com>
2021-08-07 16:49:21 +0700

    update multicall

--- src/configs/contract.ts
+++ src/configs/contract.ts
@@ -1,3 +1,13 @@
-const contract = {};
+const contract = {
+  multicall: {
+    ETH: '0x.....',
+    BSC: '0x.....',
+    BSC_TESTNET: '0x.....',
+    MATIC: '0x.....',
+    MATIC_TESTNET: '0x.....',
+    KCC: '0x.....',
+    KCC_TESTNET: '0x.....',
+  },
+};
 
 export default contract;
---
+++ src/utils/multicall.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Web3 from 'web3';
+import { Interface } from '@ethersproject/abi';
+
+import ChainIds from '../configs/chainIds';
+import { getContract } from './helper';
+import AppConfigs from '../configs';
+
+const MulticallAbi = require('../abi/MulticallAbi.json');
+
+type Call = {
+  address: string; // Address of the contract
+  name: string; // Function name on the contract (example: balanceOf)
+  params?: any[]; // Function params
+};
+
+type MulticallOptions = {
+  web3?: Web3;
+  blockNumber?: number;
+  requireSuccess?: boolean;
+};
+
+function getMulticallContract(chainId: number, web3: any): any {
+  switch (chainId) {
+    case ChainIds.BSC_TESTNET:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.BSC_TESTNET, web3);
+    case ChainIds.MATIC_TESTNET:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.MATIC_TESTNET, web3);
+    case ChainIds.MATIC_MAINNET:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.MATIC, web3);
+    case ChainIds.KCC_TESTNET:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.KCC_TESTNET, web3);
+    case ChainIds.KCC_MAINNET:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.KCC, web3);
+    case ChainIds.ETH_MAINNET:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.ETH, web3);
+    default:
+      return getContract(MulticallAbi, AppConfigs.contract.multicall.BSC, web3);
+  }
+}
+
+async function multicall(abi: any[], calls: Call[], options: MulticallOptions = {}, chainId?: number): Promise<any> {
+  try {
+    const contract = getMulticallContract(chainId || ChainIds.BSC_TESTNET, options.web3);
+    const itf = new Interface(abi);
+    const calldata = calls.map((call) => [call.address.toLowerCase(), itf.encodeFunctionData(call.name, call.params)]);
+    const { returnData } = await contract.methods.aggregate(calldata).call(undefined, options.blockNumber);
+    const res = returnData.map((call: any, i: number) => itf.decodeFunctionResult(calls[i].name, call));
+
+    return res;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default multicall;
