57a9026153ba82a01f113006cfa70dbc50c3bafb
phumaster <phumaster.dev@gmail.com>
2021-08-07 16:58:44 +0700

    update multicall

--- src/utils/multicall.ts
+++ src/utils/multicall.ts
@@ -12,7 +12,7 @@ const MulticallAbi = require('../abi/MulticallAbi.json');
 type Call = {
   address: string; // Address of the contract
   name: string; // Function name on the contract (example: balanceOf)
-  params?: any[];
+  params?: any[]; // Function params
 };
 
 type MulticallOptions = {
