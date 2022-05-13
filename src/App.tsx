29fdd5713f5730796f80d818045dfdfa48bb42a1
phumaster <phumaster.dev@gmail.com>
2022-05-13 17:14:26 +0700

    use react-dom api

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import './utils/i18n';
+import 'utils/i18n';
 
-import NotFoundPage from './modules/errors/NotFoundPage';
-import IndexPage from './modules/home/IndexPage';
-import ConnectWalletModal from './shared/components/ConnectWalletModal';
-import MainLayout from './shared/layouts/main/MainLayout';
+import NotFoundPage from 'modules/errors/NotFoundPage';
+import IndexPage from 'modules/home/IndexPage';
+import ConnectWalletModal from 'shared/components/ConnectWalletModal';
+import MainLayout from 'shared/layouts/main/MainLayout';
 
 const App: FC = () => {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<IndexPage />} />
-        <Route element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <ConnectWalletModal />
     </MainLayout>
